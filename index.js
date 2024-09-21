a22a4ab90a8f63506cf05f7f2713f3b64545f159
shubham8203 <shubhamchauhan8203@gmail.com>
2024-09-21 13:01:54 +0530

    ""

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
-    origin:'https://thedrag.in',
+    origin:'https://thedrag.in/apis',
     credentials:true,
         
 
